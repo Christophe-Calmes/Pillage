691d4cb4c9fc45eb73c2f2161c4496a6722fb49c
Calmes <christophe.calmes@laplateforme.io>
2023-04-19 17:54:46 +0200

    Formulaire des grilles

---
+++ sources/grilleFaction/CUD/CGrille.php
@@ -0,0 +1,3 @@
+<?php
+// adressage echo '<form action="'.encodeRoutage(32).'" method="post">';
+print_r($_POST);
--- sources/grilleFaction/objets/getGrilles.php
+++ sources/grilleFaction/objets/getGrilles.php
@@ -1,4 +1,16 @@
 <?php
 Class GetGrilles {
-  
+  public function getFactionData($idFaction) {
+    $param = [['prep'=> ':idFaction', 'variable'=>$idFaction]];
+    $select="SELECT `idCout`, `indexType`, `coutBase`,
+    `SP`, `armure`, `bouclier`, `armeImp`,
+    `lance`, `armeDeBase`, `hacheD`, `fronde`,
+    `javelot`, `arc`, `arbalete`, `cheval`,
+    `banniere`, `corDG`, `chienDG`, `nomFaction`, `Factions`.`idFaction`
+    FROM `cout`
+    INNER JOIN `Factions` ON `cout`.`idFaction` = `Factions`.`idFaction`
+    WHERE `cout`.`idFaction` = :idFaction";
+    $readDB = new RCUD($select, $param);
+    return $readDB->READ();
+  }
 }
