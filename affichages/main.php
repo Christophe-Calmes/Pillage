a0d506ce37eb0036c6f06090aeffbb63f020d256
Calmes <christophe.calmes@laplateforme.io>
2023-07-01 01:31:49 +0200

    Clean Code

--- affichages/main.php
+++ affichages/main.php
@@ -1,5 +1,5 @@
 <?php
-class controlerAffichage {
+class ControlerAffichage {
   // Inspiration de l'amiTik.
   public static function idOrNot($data) {
     if(isset($_SESSION['login'])) {
@@ -39,50 +39,11 @@ class controlerAffichage {
       }
   }
 }
-
 echo '<main>';
   echo '<section>';
-
-    controlerAffichage::idOrNot($_SESSION);
-
-    // if(isset($_SESSION['login'])) {
-    //   echo '<h3>Session de '.$_SESSION['login'].'</h3>';
-    // } else {
-    //   echo '<h3>Bienvenus</h3>';
-    // }
-  // Mode dev true / false - Affiche le chemin des pages + Ajout de menus / page dans le site.
-  $dev = true;
+    ControlerAffichage::idOrNot($_SESSION);
   // Affichage message
   if (isset($_GET['message'])) {echo '<h3>'.filter($_GET['message']).'</h3>';}
-
-  $idNav = controlerAffichage::displayNav($_GET, $_SESSION);
-  // Affichage elements
- // if(isset($_GET['idNav'])) {
- //    $idNav = filter($_GET['idNav']);
- //    $chemin = $readNav->getContenus($idNav);
- //    if($chemin == []) {
- //        include 'modules/navigation/pageGeneral.php';
- //
- //    } else {
- //      if($dev) {
- //        echo $chemin[0]['cheminNav'];
- //      }
- //        if(($_SESSION == [])&&($chemin[0]['niveau'] == 0)) {
- //          if($chemin[0]['niveau'] == 0) {
- //            include $chemin[0]['cheminNav'];
- //          } else {
- //              include 'modules/navigation/pageGeneral.php';
- //          }
- //        } elseif((isset($_SESSION['role']))&&($_SESSION['role'] >= $chemin[0]['niveau'])) {
- //          include $chemin[0]['cheminNav'];
- //        } else {
- //          include 'modules/navigation/pageGeneral.php';
- //        }
- //    }
- //  } else {
- //    $idNav = 0;
- //    include 'modules/navigation/pageGeneral.php';
- //  }
-
+  $idNav = ControlerAffichage::displayNav($_GET, $_SESSION);
 echo '</section>';
 echo '</main>';
